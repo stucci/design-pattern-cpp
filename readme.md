3b2aac0c257f1749c28af0dc5718161e84c0e279
stucci <sts.ucd@gmail.com>
2021-09-08 14:28:56 +0900

    visitor -> pending due to less popularity

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ```shell
 g++ -g -O0 -std=c++17 main.cc && ./a.out
 ```
+
+ref. https://refactoring.guru/design-patterns/cpp
---
+++ visitor/main.cc
@@ -0,0 +1,6 @@
+// https://refactoring.guru/design-patterns/visitor/cpp/example
+// Complexity: ★★★
+// Popularity: ★☆☆
+// Usage examples: Visitor isn’t a very common pattern because of its complexity
+// and narrow applicability.
+// -> pending
